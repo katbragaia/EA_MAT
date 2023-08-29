ac055e33f0b20425acdd968b02e8d70562ae7d71
katbragaia <142894277+katbragaia@users.noreply.github.com>
2023-08-29 08:01:56 -0300

    Add files via upload

---
+++ comparardoisnúmeros (1).c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+  int x,y;
+  printf("digite x:\n");
+  scanf("%i",&x);
+  printf("digite y:\n");
+  scanf("%i",&y);
+  if(x,y){
+     printf("x e maior \n");
+ }else{
+     if(y>x) {
+        printf("y e maior \n");
+    }else{
+        printf("x e y são iguais \n");
+         }
+      }
+
+
+
+
+}
