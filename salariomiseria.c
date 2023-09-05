b17dd04ba3ff9e35269ef30e005b3e2d53224dea
katbragaia <142894277+katbragaia@users.noreply.github.com>
2023-09-05 08:07:12 -0300

    Add files via upload

--- salariomiseria.c
+++ salariomiseria.c
@@ -1,5 +1,18 @@
 #include<stdio.h>
-int main(){
-    float VH,QHT,PD,SB,VD;
-    printf()
-}
+
+  float main(){
+
+  float V,Q,P,S,VD,SL;
+
+   printf("digite o valor da hora aula: ");
+   scanf("%f",  &V);
+   printf("digite quantidade de horas trabalhadas: ");
+   scanf("%f",   &Q);
+   printf("digite porcentagem do desconto: ");
+   scanf("%f",  &P);
+
+    S=V*Q;
+    VD=S*(P/100);
+    SL=S-VD;
+    printf("O valor do salario liquido e de: %f", SL);
+    }
