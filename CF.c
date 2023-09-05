3b7dde1c471e0afeb1d34a0c0000e6b46d390f8b
katbragaia <142894277+katbragaia@users.noreply.github.com>
2023-09-05 08:23:16 -0300

    Add files via upload

---
+++ CF.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+
+   int main(){
+
+
+   int C, F;
+
+
+   printf("digite C: \n");
+   scanf("%i", &C);
+
+
+   F=(9*C+160)/5;
+   printf("o valor em temperatura fahrenheit e de: %i", F);
+}
