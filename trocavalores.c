3472abcc5d0a20e5530faf5b0f9dc88816eb6053
katbragaia <142894277+katbragaia@users.noreply.github.com>
2023-09-12 08:39:22 -0300

    Add files via upload

---
+++ trocavalores.c
@@ -0,0 +1,22 @@
+#include<stdio.h.>
+    int main (){
+
+    int A, B, C;
+
+    printf("digite A: ");
+    scanf("%i", &A);
+
+    printf ("digite B: ");
+    scanf("%i", &B);
+
+    C=A;
+    A=B;
+    B=C;
+
+    printf("o valor de A e de: %i\n", A);
+    printf ("o velor de B e de: %i", B);
+
+
+
+}
+
