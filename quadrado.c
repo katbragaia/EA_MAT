4f71fa192533de89c72224f016dd29531e1e6264
katbragaia <142894277+katbragaia@users.noreply.github.com>
2023-09-18 16:39:19 -0300

    Add files via upload

---
+++ quadrado.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+   int main(){
+
+   int x;
+
+
+      printf("Insira um valor inteiro:\n");
+      scanf("%i",&x);
+      printf("O valor ao quadrado equivale a:%i", x*x);
+
+
+}
