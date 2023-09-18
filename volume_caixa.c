60660fc2ea7629954353abc65cc5b44a7bf17b35
katbragaia <142894277+katbragaia@users.noreply.github.com>
2023-09-18 16:25:13 -0300

    Add files via upload

---
+++ volume_caixa.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+  float main (){
+      float volume, comprimento, largura, altura;
+
+
+     printf("digite o comprimento: ");
+     scanf("%f", &comprimento);
+     printf("digite a largura: ");
+     scanf("%f", &largura);
+     printf("digite a altura: ");
+     scanf("%f", &altura);
+
+
+     volume=comprimento*largura*altura;
+     printf("o valor do volume e de: %f", volume);
+
+
+
+
+
+
+
+
+
+  }
