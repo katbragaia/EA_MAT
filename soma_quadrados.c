460b06cd69911462a82d08775302d357f79b57ab
katbragaia <142894277+katbragaia@users.noreply.github.com>
2023-09-18 17:31:04 -0300

    Add files via upload

---
+++ soma_quadrados.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+  int main (){
+
+  int A, B, C, D, E, F, G;
+
+  printf("digite o valor de A:\n");
+  scanf("%i", &A);
+  printf("digite o valor de B:\n");
+  scanf("%i", &B);
+  printf("digite o valor de C:\n");
+  scanf("%i", &C);
+
+
+  D=A*A;
+  E=B*B;
+  F=C*C;
+  G=D+E+F;
+  printf("o valor de G e de:%i", G);
+
+
+}
