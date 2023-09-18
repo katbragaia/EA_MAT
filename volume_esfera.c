d8c1e62a47783630392b95309bb7558f15d64f4a
katbragaia <142894277+katbragaia@users.noreply.github.com>
2023-09-18 20:18:34 -0300

    Add files via upload

---
+++ volume_esfera.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+ float main (){
+
+ float V, R;
+
+ printf("digite o valor do raio: \n");
+ scanf("%f", &R);
+
+ V=(4/3)*3.14159*(R*R*R);
+ printf("o volume da esfera e de:%.2f", V);
+
+
+
+
+}
