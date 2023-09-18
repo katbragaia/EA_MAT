d0ab946d97dd20ab7c38fe8ebb9d684ef5dfe969
katbragaia <142894277+katbragaia@users.noreply.github.com>
2023-09-18 20:39:49 -0300

    Add files via upload

---
+++ raiz_sem_importancia.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<math.h>
+float main (){
+float  B, N;
+double R;
+printf("digite uma base qualquer:\n");
+scanf("%f", &B);
+printf("digite um indice qualquer:\n");
+scanf("%f",&N);
+R=pow(B,(1/N));
+printf("raiz:%.2f",R);
+
+
+
+
+
+
+
+
+
+
+
+
+}
