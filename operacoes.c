1f780bb408fcf674a9e2796d0c0685ccee3dd819
katbragaia <142894277+katbragaia@users.noreply.github.com>
2023-09-18 18:40:37 -0300

    Add files via upload

---
+++ operacoes.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+  float main () {
+
+  float A, B, C, D, E, F, G, H;
+
+  printf("digite o valor de A: \n");
+  scanf("%f", &A);
+  printf("digite o valor de B: \n");
+  scanf("%f", &B);
+
+  C=A+B;
+  D=A-B;
+  E=B-A;
+  F=A*B;
+  G=A/B;
+  H=B/A;
+
+  printf("Soma= %.1f \n", C);
+  printf("Subtracao (A-B)= %.1f \n", D);
+  printf("Subtracao (B-A)= %.1f \n", E);
+  printf("Multiplicacao= %.1f \n", F);
+  printf("Divisao (A/B)= %.1f \n", G);
+  printf("Divisao (B/A)= %.1f \n", H);
+
+
+
+
+
+
+
+
+
+}
