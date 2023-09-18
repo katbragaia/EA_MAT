4f1ead094e80aee1c8d2481240c3ba866e8678be
katbragaia <142894277+katbragaia@users.noreply.github.com>
2023-09-18 17:46:41 -0300

    Add files via upload

---
+++ quadrado_das_somas.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+  int main (){
+
+  int A, B, C, D, E;
+
+
+  printf("digite o valor de A: \n");
+  scanf("%i", &A);
+  printf("digite o valor de B: \n");
+  scanf("%i", &B);
+  printf("digite o valor de C: \n");
+  scanf("%i", &C);
+
+  D=A+B+C;
+  E=D*D;
+  printf("o valor do quadrado da soma de A, B, C e de:%i", E);
+
+
+
+}
+
