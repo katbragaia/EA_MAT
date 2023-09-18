59c4b3da382d90b72cfbb01fb130e2ba58b1661d
katbragaia <142894277+katbragaia@users.noreply.github.com>
2023-09-18 18:00:14 -0300

    Add files via upload

---
+++ produto_soma.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+
+  int main (){
+
+  int A, B, C, D, P, S;
+
+
+  printf("digite o valor de A: \n");
+  scanf("%i", &A);
+  printf("digite o valor de B: \n");
+  scanf("%i", &B);
+  printf("digite o valor de C: \n");
+  scanf("%i", &C);
+  printf("digite o valor de D: \n");
+  scanf("%i", &D);
+
+  P=A*C;
+  printf("o valor do produto de A e C e de:%i\n", P);
+  S=B+D;
+  printf("o valor da soma de B e D e de:%i", S);
+
+
+
+}
