8ac56096b9c4b5065da7e46cba16242b0db2c814
katbragaia <142894277+katbragaia@users.noreply.github.com>
2023-09-18 20:25:48 -0300

    Add files via upload

---
+++ pes_metros.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+  int main()
+   {
+       float P, M;
+       printf ("digite o valor em pes: ");
+       scanf ("%f", &P);
+
+       M= P*0.3048;
+
+       printf("o valor em metros e de: %f", M);
+   }
