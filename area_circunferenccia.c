6458f254a38f71ab0048e18fbce14324f49aa11e
katbragaia <142894277+katbragaia@users.noreply.github.com>
2023-09-18 18:28:23 -0300

    Add files via upload

---
+++ area_circunferenccia.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+  float main (){
+
+  float A, R;
+
+  printf("digite o valor do raio:\n");
+  scanf("%f", &R);
+  A=3.14159265*R*R;
+  printf("o valor da area e de:\n %f", A);
+
+  }
