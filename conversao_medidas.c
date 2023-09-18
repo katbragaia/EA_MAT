f7c67885fa4c48e49a3319da4f06879c0f8b0177
katbragaia <142894277+katbragaia@users.noreply.github.com>
2023-09-18 19:40:01 -0300

    Add files via upload

---
+++ conversao_medidas.c
@@ -0,0 +1,23 @@
+ #include<stdio.h>
+ float main (){
+
+ float V,D,T;
+
+       printf("digite a distancia em km:");
+       scanf("%f",&D);
+       printf("digite o tempo em minutos:");
+       scanf("%f",&T);
+
+       V=(D*1000)/(T*60);
+
+       printf("Velocidade= %.2f m/s", V);
+
+
+
+
+
+
+
+
+
+}
