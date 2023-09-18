b347fb8e4e112e7ab7f9a80849249a9c9f676d94
katbragaia <142894277+katbragaia@users.noreply.github.com>
2023-09-18 17:19:22 -0300

    Add files via upload

---
+++ real_dolar.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+    float main(){
+
+    float conversao, reais, cotacao;
+
+     printf("Insira a cotacao atual:\n");
+     scanf("%f", &cotacao);
+     printf("digite o valor em reais:\n");
+     scanf("%f", &reais);
+     conversao=cotacao*reais;
+     printf("o valor atual da conversao em dolares e de:%.2f", conversao);
+
+
+}
+
