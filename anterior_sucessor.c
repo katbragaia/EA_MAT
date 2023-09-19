01ddeb0c41bbc9dcfe998579f1985b34ceb8738e
katbragaia <142894277+katbragaia@users.noreply.github.com>
2023-09-18 21:15:36 -0300

    Add files via upload

---
+++ anterior_sucessor.c
@@ -0,0 +1,17 @@
+ #include<stdio.h>
+
+  int main(){
+
+  int A, B, C;
+
+  printf("digite o valor de A: \n");
+  scanf("%i", &A);
+
+  B=A+1;
+  C=A-1;
+  printf("o sucessor de A e:%i\n", B);
+  printf("o antecessor de A e:%i", C);
+
+
+
+ }
