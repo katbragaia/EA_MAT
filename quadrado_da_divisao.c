c352bfca1fbcc903dd8c1f29825a86c3b882bc76
katbragaia <142894277+katbragaia@users.noreply.github.com>
2023-09-18 21:21:55 -0300

    Add files via upload

---
+++ quadrado_da_divisao.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+ int main (){
+
+ int A, B, C;
+
+  printf("digite o valor de A: \n");
+  scanf("%i", &A);
+  printf("digite o valor de B: \n");
+  scanf("%i", &B);
+
+  C=(A/B)*(A/B);
+  printf("o quadrado da divisao (A/B) e de:%i", C);
+
+
+
+}
