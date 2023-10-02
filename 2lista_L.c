35a4f7b67955638c6db9ad28ec33fe075c4b7f1f
katbragaia <142894277+katbragaia@users.noreply.github.com>
2023-10-02 16:23:55 -0300

    Add files via upload

---
+++ 2lista_L.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+ int main(){
+    int z;
+
+    printf("Digite um valor numerico inteiro: \n");
+    scanf("%i", &z);
+
+    if (!(z > 3))
+        {
+        printf("O valor digitado foi: %i", z);
+        }
+ }
