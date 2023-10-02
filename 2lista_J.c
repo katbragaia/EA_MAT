a77bc4e180811d9d8d77220db05760b8a4910305
katbragaia <142894277+katbragaia@users.noreply.github.com>
2023-10-02 16:16:20 -0300

    Add files via upload

---
+++ 2lista_J.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+
+  int main(){
+
+       int x;
+       printf("Digite um valor inteiro: \n");
+       scanf("%i",&x);
+
+       if (x%2==0)
+       {
+           printf("O valor inserido e par.");
+       }
+       else
+       {
+           printf("O valor inserido e impar.");
+       }
+   }
