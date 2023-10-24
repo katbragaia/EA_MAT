b9636028f2c4929b1de5b0eba263a437565d57cc
katbragaia <142894277+katbragaia@users.noreply.github.com>
2023-10-24 08:41:42 -0300

    Add files via upload

---
+++ laçosderepetição_E.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+int main (){
+int M, K;
+for (M=0; M<=20; M++)
+{
+
+K=M%2;
+if(K!=0)
+{
+printf("o numero e impar e seu valor e de:%i \n", M);
+
+}
+}
+
+
+
+}
