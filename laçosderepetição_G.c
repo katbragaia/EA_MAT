a601ab1dd772f62e086ae4ac8953c5ad52f8ae8d
katbragaia <142894277+katbragaia@users.noreply.github.com>
2023-10-26 10:35:55 -0300

    Add files via upload

---
+++ laçosderepetição_G.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+int main (){
+
+int M, K;
+ K=1;
+while(K<=15)
+
+{
+
+    M=pow(3, K);
+printf("o valor da potencia de base 3 e de: %i \n", M);
+
+   K=K+1;
+
+}
+
+
+}
