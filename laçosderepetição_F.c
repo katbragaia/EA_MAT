a4fd8d955ecbe49893e91d6da8df9c43fb422804
katbragaia <142894277+katbragaia@users.noreply.github.com>
2023-10-26 10:35:34 -0300

    Add files via upload

---
+++ laçosderepetição_F.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main (){
+int K, M;
+K=1;
+while(K<200)
+{
+    M=K%4;
+
+    if(M==0)
+    {
+        printf("o valor dos numeros divisiveis por 4 sao: %i\n", K);
+
+    }
+     K=K+1;
+
+}
+
+
+
+
+
+
+}
